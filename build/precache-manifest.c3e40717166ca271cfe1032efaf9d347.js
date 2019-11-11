199abab88730b374bc7f90a8f8750d2eea05e77a
Ihor Tarasenko <tarasenko.igr@gmail.com>
2019-11-11 17:07:50 +0200

    add build with new framework

---
+++ build/precache-manifest.c3e40717166ca271cfe1032efaf9d347.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8608a4314fe6123e9c3974ab5673186",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72d3ba77cb1a214343b1",
+    "url": "./static/css/2.1b47015e.chunk.css"
+  },
+  {
+    "revision": "9fb99151e44cf7b461de",
+    "url": "./static/css/main.13df46db.chunk.css"
+  },
+  {
+    "revision": "72d3ba77cb1a214343b1",
+    "url": "./static/js/2.31d5874c.chunk.js"
+  },
+  {
+    "revision": "9fb99151e44cf7b461de",
+    "url": "./static/js/main.6c192347.chunk.js"
+  },
+  {
+    "revision": "ee6fa943f84945b8c474",
+    "url": "./static/js/runtime~main.aac75caa.js"
+  },
+  {
+    "revision": "a9c69bb2ce28a02407f84503633fc85a",
+    "url": "./static/media/bg.a9c69bb2.jpg"
+  },
+  {
+    "revision": "3ac4c5b4680da8ab7ff4bc3938a06a55",
+    "url": "./static/media/bgr.3ac4c5b4.jpg"
+  },
+  {
+    "revision": "dac52b6cf7697ee926b9277830698cb4",
+    "url": "./static/media/countries.dac52b6c.csv"
+  },
+  {
+    "revision": "86ec05a0466b3370ca70b87a5b916e78",
+    "url": "./static/media/excl.86ec05a0.svg"
+  },
+  {
+    "revision": "80a4ed4719ed2aa65879060f8926fb91",
+    "url": "./static/media/flags.80a4ed47.png"
+  },
+  {
+    "revision": "7de34bf24d00cb912b2b81bfefc10d03",
+    "url": "./static/media/flags@2x.7de34bf2.png"
+  },
+  {
+    "revision": "a28e0cd99ea5da067805261ddc20c7f8",
+    "url": "./static/media/isabel.a28e0cd9.jpg"
+  },
+  {
+    "revision": "a282fe22b2093d5b38da8f0d66d57602",
+    "url": "./static/media/section4bg.a282fe22.jpg"
+  }
+]);
